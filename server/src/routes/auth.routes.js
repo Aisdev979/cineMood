15124697b0919df7efd01a13921e1fcf3f44415a
Aisdev <147014857+Aisdev979@users.noreply.github.com>
2026-05-11 11:18:39 +0100

    Add authentication protection to '/me' route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { registerUser, signInUser, verifyOtp, me, refreshToken, logout } from "../controllers/auth.controllers.js";
-//import { protect } from "../middlewares/auth.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const authRouter = Router()
 
 authRouter.post("/signup", registerUser);
 authRouter.post("/signin", signInUser);
 authRouter.post("/verify-otp", verifyOtp);
-authRouter.get("/me", me);
+authRouter.get("/me", protect, me);
 authRouter.post("/refresh", refreshToken);
 authRouter.post("/logout", logout);
 
